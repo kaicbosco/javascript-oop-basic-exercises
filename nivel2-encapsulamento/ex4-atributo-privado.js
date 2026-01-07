100f0dd58da8d2b24c0be435f8d3531f8df47c3e
kaicbosco <kaicbosco@gmail.com>
2026-01-07 09:20:39 -0300

    .

--- nivel2-encapsulamento/ex4-atributo-privado.js
+++ nivel2-encapsulamento/ex4-atributo-privado.js
@@ -2,15 +2,41 @@ class ContaBancaria {
   #saldo;
 
   constructor(saldo) {
-    this.#saldo = 0;
+    this.#saldo = saldo;
   }
 
-  depositoUsuario(valor) {
+  depositar(valor) {
+    console.log(`\nRealizando Depósito de R$${valor}...`);
     this.#saldo += valor;
-    return this.#saldo;
   }
 
-  mostrarSaldo() {
-    console.log(saldo);
+  sacar(valor) {
+    if (valor > this.#saldo) {
+      throw new Error(
+        `\nRealizando saque de R$${valor}...\nSaldo Insuficiente, interrompendo operação...`
+      );
+    }
+    this.#saldo -= valor;
+    console.log(`\nRealizando saque de R$${valor}...`);
+  }
+
+  verSaldo() {
+    console.log(`\nSaldo Atual: R$${this.#saldo}`);
   }
 }
+
+const conta1 = new ContaBancaria(1000);
+
+conta1.verSaldo();
+
+// Teste Depósito
+conta1.depositar(120);
+conta1.verSaldo();
+
+// Teste Saque
+conta1.sacar(100);
+conta1.verSaldo();
+
+// Testar saque incorreto
+conta1.sacar(2000);
+conta1.verSaldo();
